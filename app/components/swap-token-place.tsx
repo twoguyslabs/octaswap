47a116b178e35271555632b2be177dc635f5a5ae
twoguyslabs <2guyslabs@gmail.com>
2025-02-02 10:31:53 +0700

    Refactor SwapTokenPlace component to accept token props and update state handling

--- app/components/swap-token-place.tsx
+++ app/components/swap-token-place.tsx
@@ -1,20 +1,41 @@
 import { RefreshCcw } from "lucide-react";
 import { Button } from "../../components/ui/button";
 import { Separator } from "../../components/ui/separator";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { cn } from "@/lib/utils";
 
-export default function SwapTokenPlace() {
-  const [isRotated, setIsRotated] = useState(false);
+export default function SwapTokenPlace({
+  token0,
+  token1,
+  onSetToken0,
+  onSetToken1,
+}: {
+  token0: { name: string; symbol: string; logo: string };
+  token1: { name: string; symbol: string; logo: string };
+  onSetToken0: Dispatch<
+    SetStateAction<{ name: string; symbol: string; logo: string }>
+  >;
+  onSetToken1: Dispatch<
+    SetStateAction<{ name: string; symbol: string; logo: string }>
+  >;
+}) {
+  const swapTokenPlace = () => {
+    onSetToken0(token1);
+    onSetToken1(token0);
+  };
 
-  const toggleRotation = () => {
-    setIsRotated(!isRotated);
+  const [isRotated, setIsRotated] = useState(false);
+  const toggleRotation = () => setIsRotated(!isRotated);
+
+  const onClick = () => {
+    swapTokenPlace();
+    toggleRotation();
   };
   return (
     <div className="relative">
       <Separator orientation="horizontal" className="mb-9 mt-14" />
       <div className="absolute inset-0 flex items-center justify-center">
-        <Button variant="outline" size="icon" onClick={toggleRotation}>
+        <Button variant="outline" size="icon" onClick={() => onClick()}>
           <RefreshCcw
             className={cn(
               "transform transition duration-300 ease-in-out",
--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,22 @@ import SwapTokenPlace from "@/app/components/swap-token-place";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const Swap = dynamic(
   () =>
     Promise.resolve(function Swap() {
-      const token0 = {
+      const [token0, setToken0] = useState({
         name: "Ethereum",
         symbol: "ETH",
         logo: "/eth-logo.png",
-      };
+      });
 
-      const token1 = {
+      const [token1, setToken1] = useState({
         name: "Binance Smart Chain",
         symbol: "BNB",
         logo: "/bnb-logo.svg",
-      };
+      });
 
       return (
         <main>
@@ -30,7 +31,12 @@ const Swap = dynamic(
               <Card>
                 <CardContent className="px-4 py-5">
                   <SwapInput token={token0} />
-                  <SwapTokenPlace />
+                  <SwapTokenPlace
+                    token0={token0}
+                    token1={token1}
+                    onSetToken0={setToken0}
+                    onSetToken1={setToken1}
+                  />
                   <SwapInput token={token1} />
                   <Button className="mt-5 w-full">Swap</Button>
                 </CardContent>

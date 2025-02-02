d5f288b01279f2d36e17d3463c03bdbc51f6f129
twoguyslabs <2guyslabs@gmail.com>
2025-02-02 18:25:10 +0700

    Add fallback icon for tokens without logos in TokenListTrigger component

--- app/components/token-list-trigger.tsx
+++ app/components/token-list-trigger.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { DialogTrigger } from "@/components/ui/dialog";
+import { CircleHelp } from "lucide-react";
 import Image from "next/image";
 
 export default function TokenListTrigger({
@@ -14,18 +15,25 @@ export default function TokenListTrigger({
         className="absolute inset-y-1/2 right-4 -translate-y-1/2"
       >
         {token?.chainId ? (
-          <>
-            <Image
-              src={token.logoURI}
-              alt={`${token.name} Logo`}
-              width={100}
-              height={100}
-              quality={100}
-              priority
-              className="h-7 w-7"
-            />
-            <span className="text-xl font-bold">{token.symbol}</span>
-          </>
+          token.logoURI ? (
+            <>
+              <Image
+                src={token.logoURI}
+                alt={`${token.name} Logo`}
+                width={100}
+                height={100}
+                quality={100}
+                priority
+                className="h-7 w-7"
+              />
+              <span className="text-xl font-bold">{token.symbol}</span>
+            </>
+          ) : (
+            <>
+              <CircleHelp style={{ width: "1.75rem", height: "1.75rem" }} />
+              <span className="text-xl font-bold">{token.symbol}</span>
+            </>
+          )
         ) : (
           <span className="text-xl font-bold">Select token</span>
         )}

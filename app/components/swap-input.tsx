5fb458715ce623cb41f11c24ac1bf3dfeb56e192
twoguyslabs <2guyslabs@gmail.com>
2025-02-03 19:56:28 +0700

    Add debouncing to rate calculation in SwapInput for improved performance

--- app/components/swap-input.tsx
+++ app/components/swap-input.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/components/ui/input";
+import { useDebounce } from "ahooks";
 import { formatUnits } from "viem";
 
 export default function SwapInput({
@@ -12,7 +13,9 @@ export default function SwapInput({
   rateAmounts: bigint | undefined;
   elementWidth: number;
 }) {
-  const rate = rateAmounts ? formatUnits(rateAmounts, 18) : "";
+  const debouncedRate = useDebounce(rateAmounts, { wait: 300 });
+
+  const rate = debouncedRate ? formatUnits(debouncedRate, 18) : "";
   const value = amount || rate;
 
   const paddingRight = elementWidth + 37;
